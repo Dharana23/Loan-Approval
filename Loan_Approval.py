5cd2d5512e6e02a32f46c6b2c39b6648ee7aa514
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-03-20 00:13:45 +0530

    Add files via upload

---
+++ Loan_Approval.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+LoanData = pd.read_csv('C:/Users/kain_/Downloads/Course Files/007 - Classification/01Exercise1.csv')
+LoanPrep = LoanData.copy()
+
+LoanPrep.isnull().sum(axis=0)
+LoanPrep = LoanPrep.dropna()
+LoanPrep = LoanPrep.drop(['gender'], axis=1)
+
+LoanPrep = pd.get_dummies(LoanPrep, drop_first=True)
+
+scalar_ = StandardScaler()
+LoanPrep['income'] = scalar_.fit_transform(LoanPrep[['income']])
+LoanPrep['loanamt'] = scalar_.fit_transform(LoanPrep[['loanamt']])
+
+X = LoanPrep.drop(['status_Y'], axis=1)
+Y = LoanPrep['status_Y']
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=1234, stratify=Y)
+
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+y_predict = lr.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_predict)
+lr.score(X_test, y_test)
+
+ 
